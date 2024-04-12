16d00482cb6ed03cbe41c16e537b6051b10b4b52
CRRode <108227992+CRRode@users.noreply.github.com>
2024-04-11 22:20:13 -0400

    Update shader.h

--- shader.h
+++ shader.h
@@ -1,3 +1,8 @@
+//Christopher Rode
+//Date: 3/31/24
+//version 2.1
+//this code is responsible for setting up our shaders and is retreveing data from our shader files. 
+
 #ifndef SHADER_H
 #define SHADER_H
 
