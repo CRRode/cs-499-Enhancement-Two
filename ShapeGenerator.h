d05449cfa279adeb45f9d6a31a62b78eeecdcd5c
CRRode <108227992+CRRode@users.noreply.github.com>
2024-04-11 22:16:38 -0400

    Update ShapeGenerator.h

--- ShapeGenerator.h
+++ ShapeGenerator.h
@@ -1,3 +1,8 @@
+//Christopher Rode
+//Date: 3/31/24
+//version 2.1
+//this code is to make our shape data for our plane and sphere verts and indices
+
 #pragma once
 #include "ShapeData.h"
 typedef unsigned int uint;
