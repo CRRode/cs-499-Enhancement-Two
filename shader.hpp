35fbba83dc5080c66dd72c5812b8b90609f4f8df
CRRode <108227992+CRRode@users.noreply.github.com>
2024-04-11 22:20:27 -0400

    Update shader.hpp

--- shader.hpp
+++ shader.hpp
@@ -1,3 +1,8 @@
+//Christopher Rode
+//Date: 3/31/24
+//version 2.1
+//this header file is used to declare the 'loadShaders' function which can then be defined in a corresponding source file (shader.cpp)
+
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
