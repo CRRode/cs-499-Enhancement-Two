b9127137167569fc802df371650f34e1418c0a6c
CRRode <108227992+CRRode@users.noreply.github.com>
2024-04-11 22:18:16 -0400

    Update cylinder.h

--- cylinder.h
+++ cylinder.h
@@ -1,3 +1,8 @@
+//Christopher Rode
+//Date: 3/31/24
+//version 2.1
+//This class provides functionality to create and render a cylinder in a 3D scene, allowing customization of its dimensions and appearance.
+
 #pragma once
 #include "staticMesh3D.h"
 
